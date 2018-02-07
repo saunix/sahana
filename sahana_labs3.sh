e05801f27ff05728c545bb528225f30424ac141f
saunix <35552248+saunix@users.noreply.github.com>
2018-02-07 10:37:25 +0530

    Update sahana_labs3.sh

--- sahana_labs3.sh
+++ sahana_labs3.sh
@@ -1,2 +1,17 @@
+echo "today date is"
+ date +%D
+echo time 
+date +%r
+echo year
+date +%y
+
+echo "enter a number"
+read number
+if [ $number -ge 0 ]
+then
+        echo"positive"
+else
+        echo "negative"
+fi
 echo s= "lolol"
 echo $s
